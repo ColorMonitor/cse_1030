a9a7fc8ea399d71db24a45f93aa8ef450e7de5f2
Ye Min Aung <yeminaung013@gmail.com>
2022-08-19 18:55:01 +0630

    cse 1020 js exercise

---
+++ Web/CSE _020_Session_9A.js
@@ -0,0 +1,15 @@
+/* ********************************* */
+/* CSE 1020 Session 9A */
+
+// The Fortune Teller
+function tellFortune(jobTtle, geographicLocation, partner, numKids) {
+    console.log('You will be ' + jobTtle + ' in ' + geographicLocation + ' and married to ' + partner + ' with ' + numKids + ' kids.');
+}
+
+/* tellFortune("web developer", "US", "Emily", 2);
+tellFortune("web designer", "UK", "Cherry", 3);
+tellFortune("fullstack developer", "Singapore", "Jane", 1); */
+
+// The Puppy Age Calculator
+
+// The Lifetime Supply Calculator
